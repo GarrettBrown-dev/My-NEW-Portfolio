1e20ff84427a3880f1b57026b863a27df3fb9d7d
Garrett <gman9mm@live.com>
2021-06-18 12:23:30 -0700

    Modify routing for imports.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import "/css/style.css";
-import * as THREE from "three";
+import * as THREE from "./node_modules/three";
 //import { GridHelper } from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { OrbitControls } from "./node_modules/three/examples/jsm/controls/OrbitControls";
 
 const scene = new THREE.Scene();
 
